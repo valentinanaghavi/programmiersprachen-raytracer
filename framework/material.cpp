f33c08307073f2e066bf7e30f4b2276f70560790
valentinanaghavi <gissu.valentina.naghavi@uni-weimar.de>
2018-07-07 17:40:30 +0200

    add 6.4 without std::shared_ptr<Material>

---
+++ framework/material.cpp
@@ -0,0 +1,34 @@
+#include "material.hpp"
+
+#include <iostream>
+
+Material::Material() :
+    name_ {"no Name"},
+    ka_ {0.0f, 0.0f, 0.0f}, 
+    kd_ {0.0f, 0.0f, 0.0f},
+    ks_ {0.0f, 0.0f, 0.0f},
+    m_ {0.0f}
+    {};
+
+Material::Material(std::string const& name, Color const& ka, Color const& kd, Color const& ks, float m) :
+    name_ {name},
+    ka_ {ka},
+    kd_ {kd},
+    ks_ {ks},
+    m_ {m}
+    {};
+
+
+std::ostream& Material :: print(std::ostream& os ) const 
+{
+    os << "Name: " << name_ << "\n"
+	<< "ambient reflection: " << ka_
+	<< "diffuse reflection: " << kd_
+	<< "specular reflection: " << ks_
+	<< "specular reflection exponent: " << m_ << " \n";
+    return os;
+
+}
+
+
+
---
+++ framework/material.hpp
@@ -0,0 +1,27 @@
+#ifndef MATERIAL_HPP
+#define MATERIAL_HPP
+
+#include <string>
+#include "color.hpp"
+
+struct Material
+{
+    //constructors
+    Material();
+    Material(std::string const& name, Color const& ka, Color const& kd, Color const& ks, float m);
+
+    //function
+    std::ostream& print(std::ostream& os ) const ;
+
+    //members
+    std::string name_;
+    Color ka_;
+    Color kd_;
+    Color ks_;
+    float m_;
+
+};
+//free function
+std::ostream& operator << (std::ostream& os, Material const& mat);
+
+#endif //define MATERIAL_HPP
